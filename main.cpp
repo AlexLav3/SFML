f80b4d8ef72c9f247ab19a35728bde7029a95eac
AlexLav3 <151187004+AlexLav3@users.noreply.github.com>
2024-03-31 22:34:44 +0200

    basic Move/mouse move/player

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Tutorial", sf::Style::Default ); 
+    sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f)); 
+    player.setFillColor(sf::Color::Cyan); 
+    player.setOrigin(50.0f, 50.f); 
 
     while (window.isOpen()) {
 
@@ -29,6 +32,35 @@ int main()
 
             }
         }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            
+            player.move(-0.1f, 0.0f); 
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+
+            player.move(0.1f, 0.0f);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+
+            player.move(0.0f, -0.1f);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+
+            player.move(0.0f, 0.01f);
+        }
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+
+            sf::Vector2i MousePos = sf::Mouse::getPosition(window);
+            player.setPosition((float)MousePos.x, static_cast<float>(MousePos.y));
+        }
+        window.clear(); 
+        window.draw(player); 
+        window.display(); 
     }
     return 0;
 }
